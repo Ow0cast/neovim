012a5cc36eb308ff9134b594a48211fc2a7853c0
owuh <owuh@wayland.best>
2025-06-21 03:25:09 +0000

    feat(): dash, completion, and formatting

--- init.lua
+++ init.lua
@@ -2,24 +2,27 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-		      "git",
-		      "clone",
-		      "--filter=blob:none",
-		      "https://github.com/folke/lazy.nvim.git",
-		      "--branch=stable", -- latest stable release
-		      lazypath,
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- set leader
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- init plugins
 require("lazy").setup("plugins")
 
 -- general config
-vim.cmd 'set tabstop=2'
-vim.cmd 'set softtabstop=2'
-vim.cmd 'set shiftwidth=2'
-vim.cmd 'set number'
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+
+-- suck it readonly files
+vim.cmd("cmap w!! %!sudo tee > /dev/null %")
---
+++ lua/plugins/dash.lua
@@ -0,0 +1,35 @@
+return {
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = {
+			[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠲⠶⣦⣄⠀⠀⠀⠀]],
+			[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣷⣄⠀⠀]],
+			[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣄⠀]],
+			[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⢀⣼⠿⠛⢻⣆]],
+			[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠀⠀⠀⠀⠀⠀⠈⠀⠀⢀⣼⡿]],
+			[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣇⠀⠀⠀⠀⢀⣀⣠⣶⣶⣿⣿⠃]],
+			[[⢀⣀⣀⣀⣠⣤⣤⣤⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠃⠀]],
+			[[⠈⠛⢿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀]],
+			[[⠀⠀⠀⠐⠺⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠟⠋⠉⠀⠀⠀⠀⠀⠀⠀]],
+			[[⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⢹⣿⣿⡿⢿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+			[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⡟⠀⠀⠈⠛⠿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+			[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("r", "  Recent"  , ":Telescope oldfiles<CR>"),
+			dashboard.button("f", "  Search"  , ":Telescope find_files<CR>"),
+			dashboard.button("q", "  Quit"    , ":qa<CR>"),
+		}
+
+		alpha.setup(dashboard.config)
+	end,
+}
